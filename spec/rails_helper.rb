98efffa5d5139a7817377ba760526352ffb53c8d
KenpachiZar7 <giokorompilis@gmail.com>
2026-02-17 19:19:53 +0200

    Initial Commit

---
+++ spec/rails_helper.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+require 'database_cleaner'
+
+# MONKEY PATCH για συμβατότητα με Rails 7/8
+module RSpec
+  module Core
+    class ExampleGroup
+      def self.fixture_path=(path); end
+      def self.fixture_paths=(paths); end
+    end
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
